2a577a8af6f21feb1e446da8f5b4a6fdfe11ad56
Prajapati Yash <77118917+prajapati-yash@users.noreply.github.com>
2023-08-02 21:25:59 +0530

    updating change

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -16,9 +16,6 @@ const navigation = [
 
 export default function Navbar() {
   const [scrolled, setScrolled] = useState(false);
-  
-
-
   useEffect(()=>{
     const handleScroll = () => {
       if (window.scrollY > 50) {
@@ -32,10 +29,7 @@ export default function Navbar() {
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
-
-
- 
+  }, []); 
   return (
     <>
       <nav
