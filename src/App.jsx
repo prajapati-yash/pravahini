33f33bbfd8a48e55cc6d1c21715c18702ffb7257
Prajapati Yash <77118917+prajapati-yash@users.noreply.github.com>
2023-08-03 15:23:33 +0530

    Made change routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import Home from "./pages/Home";
 import Registration from "./pages/Registration";
 import Dashboard from "./pages/Dashboard";
 import { useState } from "react";
 import "./App.css";
 
+
+const router = createBrowserRouter(
+  createRoutesFromElements( <>
+    <Route path="/" element={<Home />}/>
+    <Route path="/register" element={<Registration />}/>
+    <Route path="/user-dashboard" element={<Dashboard />}/>
+    <Route path="/user-dashboard/*" element={<Dashboard />}/></>
+  )
+)
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="app-container">
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/register" render={()=> <Registration />}/>
           <Route path="/user-dashboard" element={<Dashboard />}></Route>
           <Route path="/user-dashboard/*" element={<Dashboard />}></Route>
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <RouterProvider router={router}/>
     </div>
   );
 }
