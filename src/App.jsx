7f6a9c76e3f1d5326d6fe1460efc1d16c57e24b5
Prajapati Yash <77118917+prajapati-yash@users.noreply.github.com>
2023-08-03 14:55:48 +0530

    changs in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-=======
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
->>>>>>> 21d6b11b39e49e2661548afd6b7564060e753aa7
 import Home from "./pages/Home";
 import Registration from "./pages/Registration";
 import Dashboard from "./pages/Dashboard";
@@ -14,7 +10,6 @@ function App() {
 
   return (
     <div className="app-container">
-<<<<<<< HEAD
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
@@ -23,19 +18,6 @@ function App() {
           <Route path="/user-dashboard/*" element={<Dashboard />}></Route>
         </Routes>
       </BrowserRouter>
-=======
-    <Router>
-
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/register" element={<Registration/>}/>
-        <Route path="/user-dashboard" element={<Dashboard/>}></Route>
-        <Route path="/user-dashboard/*" element={<Dashboard/>}></Route>
-      </Routes>
-    </Router>
-      
-
->>>>>>> 21d6b11b39e49e2661548afd6b7564060e753aa7
     </div>
   );
 }
