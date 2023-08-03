186063d572db19f53c9a9306d67cb10d6ba59420
Prajapati Yash <77118917+prajapati-yash@users.noreply.github.com>
2023-08-03 14:51:45 +0530

    changes in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Registration from "./pages/Registration";
 import Dashboard from "./pages/Dashboard";
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="app-container">
-    <BrowserRouter>
-
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/register" element={<Registration/>}></Route>
-        <Route path="/user-dashboard" element={<Dashboard/>}></Route>
-        <Route path="/user-dashboard/*" element={<Dashboard/>}></Route>
-      </Routes>
-    </BrowserRouter>
-      
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/register" render={()=> <Registration />}/>
+          <Route path="/user-dashboard" element={<Dashboard />}></Route>
+          <Route path="/user-dashboard/*" element={<Dashboard />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
