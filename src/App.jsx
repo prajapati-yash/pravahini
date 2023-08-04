9352d1ef2f14de53001d06474a970a20e6b8a435
Prajapati Yash <77118917+prajapati-yash@users.noreply.github.com>
2023-08-04 09:28:07 +0530

    For routing created different component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,20 @@
 // import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
-import Home from "./pages/Home";
-import Registration from "./pages/Registration";
-import Dashboard from "./pages/Dashboard";
-import { useState } from "react";
-import "./App.css";
+// import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+// 
+
+import AppRouter from './router';
+import './App.css';
 
 
-const router = createBrowserRouter([
-  {path:"/", element:<Home/>},{path:"register", element:<Registration/>},{path:"user-dashboard", element:<Dashboard/>}
-]
-)
+// const router = createBrowserRouter([
+//   {path:"/", element:<Home/>},{path:"register", element:<Registration/>},{path:"user-dashboard", element:<Dashboard/>}
+// ]
+// )
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="app-container">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/register" render={()=> <Registration />}/>
-          <Route path="/user-dashboard" element={<Dashboard />}></Route>
-          <Route path="/user-dashboard/*" element={<Dashboard />}></Route>
-        </Routes>
-      </BrowserRouter>
-      {/* <RouterProvider router={router}/> */}
+      <AppRouter />
     </div>
   );
 }
---
+++ src/router.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from "./pages/Home";
+import Registration from "./pages/Registration";
+import Dashboard from "./pages/Dashboard";
+
+const AppRouter = () => {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" component={Home}/>
+          <Route path="/register" compponent={Registration}/>
+          <Route path="/user-dashboard" component={Dashboard}></Route>
+          <Route path="/user-dashboard/*" component={Dashboard}></Route>
+        </Switch>
+      </Router>
+    );
+  };
