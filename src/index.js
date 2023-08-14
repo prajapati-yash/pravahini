60abf4375c25d95d1cc14811eb01f490f020b197
isha-mistry <ishamistry.lamprostech@gmail.com>
2023-08-14 11:22:15 +0530

    Made changes in navbar

--- src/index.js
+++ src/index.js
@@ -2,11 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import {
+  polygonMumbai,
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  zora,
+} from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient } = configureChains(
+  [polygonMumbai, mainnet, polygon, optimism, arbitrum, zora],
+  [
+    alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-      <App />
+      <WagmiConfig config={wagmiConfig}>
+        <RainbowKitProvider chains={chains} theme={darkTheme()} coolMode>
+          <App />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </React.StrictMode>
    
 );
--- src/pages/Model.js
+++ src/pages/Model.js
@@ -1,14 +1,26 @@
 import React from 'react'
-import CreateModel from '../components/model/CreateModel'
 import ModelDashboard from '../components/model/ModelDashboard'
+import Sidebar from '../components/sidebar/Sidebar'
+import Navbar from '../components/navbar/Navbar'
+import Footer from '../components/footer/Footer'
 
 function Model() {
   return (
     <div style={{ background: "linear-gradient(to right, #E9F1FF, #B5D2FE)" }}>
-        {/* <Navbar /> */}
-        {/* <CreateModel /> */}
-        <ModelDashboard />
-        {/* <Footer /> */}
+      <Navbar />
+      <div className="app">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-3">
+              <Sidebar />
+            </div>
+            <div className="col-lg-9">
+              <ModelDashboard />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   )
 }
