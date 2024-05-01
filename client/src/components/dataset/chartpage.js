5176bf3f6f7d39f5861110c9866fd2a19e409770
isha-bagadiya <ishabagadiya@lampros.tech>
2024-05-01 10:46:38 +0530

    Bar Chart tooltip is rounded to 2 decimal

--- client/src/components/dataset/chartpage.js
+++ client/src/components/dataset/chartpage.js
@@ -253,6 +253,12 @@ const Chartss = () => {
         </div>
       );
     }
+    const customTooltipFormatter = (value, name, props) => {
+      if (typeof value === 'number') {
+        return `${value.toFixed(2)}`;
+      }
+      return value;
+    };
 
     switch (chartType) {
       case "bar":
@@ -269,7 +275,7 @@ const Chartss = () => {
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey={xAxisColumn}/>
             <YAxis />
-            <Tooltip  />
+            <Tooltip formatter={customTooltipFormatter} />
             <Legend />
             <ReferenceLine y={0} stroke="#000" />
             <Brush dataKey={xAxisColumn} height={30} stroke="#8884d8" />
